7aa3ce5bc3eb4fbff55a49140fcbb393fcba8256
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-02-20 10:31:19 +0300

    Update 4

--- src/Components/Pages/Main_Page.jsx
+++ src/Components/Pages/Main_Page.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import CenterBlock from '../Standart/CenterBlock/CenterBlock'
-import backgroundMainImg from "../../../public/backgroundMainImg.png"
+import ColumnBlock from '../Standart/ColumnBlock/ColumnBlock'
+import { Link } from "react-router-dom";
+
+import backgroundMainImg from "../../assets/backgroundMainImg.png"
+import logo from "../../assets/герб.png"
+import teaName from "../../assets/ДЕНЬ ГОРНОГО ЧАЯ.png"
+import place from "../../assets/place.png"
+import Button from "../Standart/Button/Button";
 
 function Main_Page({ children, ...props }) {
     return (
@@ -10,8 +17,26 @@ function Main_Page({ children, ...props }) {
                 height={'100vh'}
                 background={backgroundMainImg}
             >
-                
-            </CenterBlock>
+                <ColumnBlock
+                    width={'900px'}
+                    gap={'50px'}
+                    justifyContent={'center'}
+                >
+                    <CenterBlock>
+                        <img src={logo} alt="" style={{ width: '186px' }} />
+                    </CenterBlock>
+                    <CenterBlock>
+                        <img src={teaName} alt="" style={{ width: '669px' }} />
+                    </CenterBlock>
+                    <CenterBlock>
+                        <Button width={'319px'} height={'60px'}>
+                            <img src={place} alt="" />
+                            Домбай
+                        </Button>
+                    </CenterBlock>
+
+                </ColumnBlock >
+            </CenterBlock >
         </>
     );
 }
---
+++ src/Components/Standart/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from './Button.module.css';
+import { Link } from "react-router-dom";
+
+function Button({ children, ...props }) {
+    return (
+        <>
+            <Link href={props.link} className={classes.Button} style={{ width: props.width, height: props.height }}>
+                {children}
+            </Link>
+        </>
+    );
+}
+
+export default Button;
