bc0ab7ee19efa0d4b4a8c1023e7caa8a4a614f91
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-02-20 12:55:38 +0300

    Update 6

--- src/Components/Blocks/Footer/Footer.jsx
+++ src/Components/Blocks/Footer/Footer.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import classes from './Footer.module.css';
+import RowBlock from "../../Standart/RowBlock/RowBlock";
+import CenterBlock from "../../Standart/CenterBlock/CenterBlock";
+import ColumnBlock from "../../Standart/ColumnBlock/ColumnBlock";
 
 function Footer({ children, ...props }) {
-    return ( 
+    return (
         <>
-            Footer
+            <div className={classes.Footer}>
+                <CenterBlock>
+                    <ColumnBlock width={'100%'}>
+                        <CenterBlock gap={'50px'}>
+                            <div className={classes.Footer_line}></div>
+                            <ColumnBlock>
+                                <CenterBlock gap={'20px'}>
+                                    <div className={classes.Footer_link}>Политика конфиденциальности</div>
+                                    <div className={classes.Footer_link}>Согласие на обработку персональных данных</div>
+                                    <div className={classes.Footer_link}>Контактный номер 8000000000</div>
+                                </CenterBlock>
+                            </ColumnBlock>
+                        </CenterBlock>
+                    </ColumnBlock>
+                </CenterBlock >
+            </div >
         </>
-     );
+    );
 }
 
 export default Footer;
