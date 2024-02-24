afb8c532a754d1c19da20dbb2c2f91a8ff11041b
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-02-24 12:33:50 +0300

    Update

--- src/Components/Pages/Main_Page.jsx
+++ src/Components/Pages/Main_Page.jsx
@@ -25,7 +25,7 @@ function Main_Page({ children, ...props }) {
       <main>
         <CenterBlock
           width={"100%"}
-          height={"100vh"}
+          height={"50vh"}
           background={backgroundMainImg}
           gap={"70px"}
         >
@@ -46,50 +46,18 @@ function Main_Page({ children, ...props }) {
                 className="logoText"
               />
             </CenterBlock>
-            <CenterBlock>
-              <Button width={"319px"} height={"60px"} link={'https://yandex.ru/maps/-/CDBlqCPF'}>
-                <svg
-                  width="16"
-                  height="22"
-                  viewBox="0 0 16 22"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M7.99996 21.5C6.55646 20.2433 5.21847 18.8656 3.99998 17.3814C2.17142 15.1523 1.00989e-06 11.8327 1.00989e-06 8.66931C-0.00161974 5.36547 1.9477 2.38623 4.93834 1.12183C7.92898 -0.142565 11.3714 0.5571 13.6594 2.89434C15.1639 4.42306 16.0067 6.50255 16 8.66931C16 11.8327 13.8285 15.1523 11.9999 17.3814C10.7814 18.8656 9.44346 20.2433 7.99996 21.5ZM7.99996 5.17003C6.77506 5.17003 5.6432 5.83699 5.03075 6.91967C4.41829 8.00235 4.41829 9.33627 5.03075 10.419C5.6432 11.5016 6.77506 12.1686 7.99996 12.1686C9.8935 12.1686 11.4285 10.6019 11.4285 8.66931C11.4285 6.73671 9.8935 5.17003 7.99996 5.17003Z"
-                    fill="white"
-                  />
-                </svg>
-                Домбай
-              </Button>
-            </CenterBlock>
           </ColumnBlock>
-
-          <CenterBlock>
-            <RowBlock width={"100%"} gap={"100px"}>
-              <img
-                src={partner1}
-                alt=""
-                style={{ width: "352px", height: "100px" }}
-                className="partner_logo"
-              />
-              <img
-                src={partner2}
-                alt=""
-                style={{ width: "352px", height: "100px" }}
-                className="partner_logo"
-              />
-            </RowBlock>
-          </CenterBlock>
         </CenterBlock>
 
         <img
           src={mainBlockLineImg}
           alt=""
           width={"100%"}
-          style={{ marginTop: "-81px" }}
+          style={{ marginTop: "-81px", zIndex: "9999" }}
           className="bottomImgLine"
         />
+
+        <Chat />
 
         <CenterBlock width={"100%"} id="programm">
           <ColumnBlock width={"var(--blockWidth)"} gap={"80px"}>
@@ -101,7 +69,7 @@ function Main_Page({ children, ...props }) {
                   title={"10:00 - 15:00"}
                   body={"Дегустация горного чая.  Детская анимационная программа. Авторский сет от диджея Mr.As - 'Танцевальный склон'"}
                   color={"#fff"}
-                  background_color={"#007030"}                  
+                  background_color={"#007030"}
                   link={'https://yandex.ru/maps/-/CDBpiU0D'}
                   split={'split'}
                 />
@@ -214,7 +182,7 @@ function Main_Page({ children, ...props }) {
           </ColumnBlock>
         </CenterBlock>
 
-        <Chat />
+
 
         <CenterBlock id="chat">
           <ColumnBlock
